2d5bf18e058ee8dca478e5c2ebb3205ae93fb1c3
Chaeeun-Lee <39520703+Chaeeun-Lee@users.noreply.github.com>
2018-09-12 10:51:04 +0900

    onclick 동작하지 않는 것 수정

--- JS/toggle/new toggle/check.js
+++ JS/toggle/new toggle/check.js
@@ -1,32 +1,30 @@
 let chs = {
-    ch1 = 0,
-    ch2 = 0
+    ch1: 0,
+    ch2: 0
 };
 
 function TheBoyz() {
     let img1 = './theboyz1.jpg';
     let img2 = './theboyz2.jpg';
     let target = document.getElementById("theBoyz");
-    let channel = chs.ch1;
     console.log('tbz onclicked');
-    toggle(img1, img2, target, channel);
+    chs.ch1 = toggle(img1, img2, target, chs.ch1);
 }
 
 function Hwall() {
     let img1 = './hwall1.jpg';
     let img2 = './hwall2.jpg';
     let target = document.getElementById("hwall");
-    let channel = chs.ch2;
     console.log('hwall onclicked');
-    toggle(img1, img2, target, channel);
+    chs.ch2 = toggle(img1, img2, target, chs.ch2);
 }
 
 function toggle(img1, img2, target, channel) {
     if (channel === 0) {
         target.setAttribute("src", img2);
-        channel = 1;
+        return 1;
     } else {
         target.setAttribute("src", img1);
-        channel = 0;
+        return 0;
     }
 }
