cc260f8d635f5a4a81008c6ba31e919c15d145e3
Chaeeun-Lee <lce4376@gmail.com>
2018-10-10 15:02:04 +0900

    이진트리 중위 순회, 후위 순회, 전위 순회 추가

--- 자료구조와 알고리즘/BinaryTree.c
+++ 자료구조와 알고리즘/BinaryTree.c
@@ -47,6 +47,33 @@ void MakeRightSubTree(BTreeNode * main, BTreeNode * sub)
 
 	main->right = sub;
 }
+void InorderTraverse(BTreeNode * bt)
+{
+	if (bt == NULL)
+		return;
+
+	InorderTraverse(bt->left);
+	printf("%d ", bt->data);
+	InorderTraverse(bt->right);
+}
+void PostorderTraverse(BTreeNode * bt)
+{
+	if (bt == NULL)
+		return;
+
+	PostorderTraverse(bt->left);
+	PostorderTraverse(bt->right);
+	printf("%d ", bt->data);
+}
+void PreorderTraverse(BTreeNode * bt)
+{
+	if (bt == NULL)
+		return;
+
+	printf("%d ", bt->data);
+	PreorderTraverse(bt->left);
+	PreorderTraverse(bt->right);
+}
 
 int main()
 {
@@ -57,6 +84,7 @@ int main()
 	BTreeNode * E = MakeBTreeNode();
 	BTreeNode * F = MakeBTreeNode();
 	BTreeNode * G = MakeBTreeNode();
+	BTreeNode * H = MakeBTreeNode();
 
 	SetData(A, 1);
 	SetData(B, 2);
@@ -65,6 +93,7 @@ int main()
 	SetData(E, 5);
 	SetData(F, 6);
 	SetData(G, 7);
+	SetData(H, 8);
 
 	MakeLeftSubTree(A, B);
 	MakeRightSubTree(A, C);
@@ -72,18 +101,35 @@ int main()
 	MakeRightSubTree(B, E);
 	MakeLeftSubTree(C, F);
 	MakeRightSubTree(C, G);
+	MakeLeftSubTree(D, H);
 
 	// 포화이진트리 센터화해서 출력
-	printf("%4d\n", GetData(A));
-	printf("%2d", GetData(GetLeftSubTree(A)));
-	printf("%4d\n", GetData(GetRightSubTree(A)));
-	printf("%d ", GetData(GetLeftSubTree(GetLeftSubTree(A))));
-	printf("%d ", GetData(GetRightSubTree(GetLeftSubTree(A))));
-	printf("%d ", GetData(GetLeftSubTree(GetRightSubTree(A))));
-	printf("%d \n", GetData(GetRightSubTree(GetRightSubTree(A))));
+	printf("%6d\n", GetData(A));
+	printf("%4d", GetData(B));
+	printf("%5d\n", GetData(C));
+	printf("%2d ", GetData(D));
+	printf("%2d ", GetData(E));
+	printf("%2d ", GetData(F));
+	printf("%2d \n", GetData(G));
+	printf("%d\n", GetData(H));
 
-	// 루트 노드부터 시작해 오른쪽에 있는 노드들만 출력
-	printf("\n%d\n", GetData(A));
-	printf("%d\n", GetData(GetRightSubTree(A)));
-	printf("%d\n", GetData(GetRightSubTree(GetRightSubTree(A))));
+	// 중위 순회로 데이터 출력
+	printf("\n중위 순회로 데이터 출력\n");
+	InorderTraverse(A);
+	printf("\n");
+
+	// 후위 순회로 데이터 출력
+	printf("\n후위 순회로 데이터 출력\n");
+	PostorderTraverse(A);
+	printf("\n");
+
+	// 전위 순회로 데이터 출력
+	printf("\n전위 순회로 데이터 출력\n");
+	PreorderTraverse(A);
+	printf("\n");
+
+	//// 루트 노드부터 시작해 오른쪽에 있는 노드들만 출력
+	//printf("\n%d\n", GetData(A));
+	//printf("%d\n", GetData(GetRightSubTree(A)));
+	//printf("%d\n", GetData(GetRightSubTree(GetRightSubTree(A))));
 }
